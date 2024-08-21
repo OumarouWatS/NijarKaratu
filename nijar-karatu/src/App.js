dc979c6c9f89a384542451577ef454bf886a7917
OumarouWatS <OumarouWatS@jbu.edu>
2024-08-21 01:22:33 -0500

    new react set up after understanding the issue with my previous set up.

---
+++ nijar-karatu/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>The react set up with git finally works smoothly!!</h1>
+        <h2>I'll migrate the old code here along with database set up very soon!!</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
